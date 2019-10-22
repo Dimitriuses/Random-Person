8d42c8d7feab978824a77a6f816b470f60d0fb74
Dmitriuses <shpacosky.dmutro.1999.mc@gmail.com>
2019-10-22 11:28:03 +0300

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
+import { PersonListComponent } from './person/person-list/person-list.component';
+import { PersonDetailComponent } from './person/person-detail/person-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    PersonListComponent,
+    PersonDetailComponent
   ],
   imports: [
     BrowserModule
